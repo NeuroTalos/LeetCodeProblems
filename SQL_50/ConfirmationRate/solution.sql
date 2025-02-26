c3bfc766254e0bc78771a5a199747284a2bbc22d
NeuroTalos <neurotalos.26@yahoo.com>
2025-02-26 09:30:31 +0800

    Added one SQL problem

---
+++ SQL_50/ConfirmationRate/solution.sql
@@ -0,0 +1,10 @@
+SELECT S.user_id, 
+       COALESCE(
+           ROUND(
+               (COUNT(CASE WHEN C.action = 'confirmed' THEN 1 END) * 1.0) 
+               / NULLIF(COUNT(C.action), 0), 2
+           ), 0) AS confirmation_rate
+FROM Signups S
+LEFT JOIN Confirmations C
+  ON S.user_id = C.user_id
+GROUP BY S.user_id;
